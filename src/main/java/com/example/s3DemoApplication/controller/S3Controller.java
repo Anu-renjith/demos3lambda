986c5220c6bb2f5f77b7d4fc9deb487c3a914ea5
Anu <anukunjumon7.vsk@gmail.com>
2026-03-04 11:45:39 +0530

    upload and download file functions added in controller

--- src/main/java/com/example/s3DemoApplication/controller/S3Controller.java
+++ src/main/java/com/example/s3DemoApplication/controller/S3Controller.java
@@ -1,4 +1,30 @@
 package com.example.s3DemoApplication.controller;
 
+import com.example.s3DemoApplication.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
 public class S3Controller {
+    @Autowired
+    private S3Service s3Service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) throws Exception {
+        s3Service.uploadFile(file);
+        return ResponseEntity.ok("file uploaded successfully");
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<byte[]> download(@PathVariable String fileName) {
+        byte[] data = s3Service.downloadFile(fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;fileName=" + fileName)
+                .body(data);
+    }
 }
